ff6b5f79baadf1726ff93bc14aad344a39086634
Noa B <noaboserup@gmail.com>
2026-02-02 11:26:08 +0100

    Initialized functions and classes

--- main.py
+++ main.py
@@ -2,3 +2,26 @@ import pygame as pg
 import pygame.display as disp
 import pygame.freetype as ft
 import pygame.colordict as color
+
+def toggle():
+    return
+
+class Textbox:
+    def x():
+        return
+
+class Button:
+    def x():
+        return
+
+def show_level():
+    return
+
+def won():
+    return
+
+def main_screen():
+    return
+
+def main():
+    return
