ff72b9196ca8488cd4c43cab347a7bcdd2e26c34
Noa B <noaboserup@gmail.com>
2026-02-04 16:52:34 +0100

    Implemented the Textbox class (not including checking answer). Also implemented the rendering of these

--- main.py
+++ main.py
@@ -18,21 +18,43 @@ BLACK = pg.Color("black")
 global FONT1
 FONT1 = ft.SysFont("Calibri", 20, False, False)
 
-def toggle(var: bool):
-    if var == False:
-        var = True
-    else:
-        var = False
-
 class Textbox:
-    def __init__(self, x=int, y=int, width=int, height=int):
+    def __init__(self, x=int, y=int, width=int, height=int, textcolor=pg.Color, screen=pg.Surface):
         self.x = x
         self.y = y
         self.rect = pg.Rect(x, y, width, height)
-        self.textfield
+        self.textcolor = textcolor
+        self.screen = screen
+        
+        self.color = INACTIVE
+        self.active = False
 
-    def x():
+        self.text, self.textrect = "", pg.rect
+        self.textfield = pg.Surface
+
+    def draw(self):
+        self.textfield, self.textrect = FONT1.render(self.text, self.textcolor)
+        self.screen.blit(self.textfield, self.rect)
+        pg.draw.rect(self.screen, self.color, self.rect)
+
+    def check_events(self, event):
+        if event.type == pg.MOUSEBUTTONDOWN and self.rect.collidepoint(event.pos): # Håndterer aktivering af klik. Kan laves mere effektiv?
+            self.active = not self.active
+        if event.type == pg.MOUSEBUTTONDOWN and not self.rect.collidepoint(event.pos):
+            self.active = False
+        self.color = ACTIVE if self.active else INACTIVE
+
+        if event.type == pg.KEYDOWN and self.active:
+            if event.key not in (pg.K_RETURN, pg.K_SPACE, pg.K_BACKSPACE):
+                self.text = event.unicode
+            if event.key == pg.K_RETURN:
+                self.checkcorrect()
+        
+        self.draw()
+    
+    def checkcorrect(self):
         return
+        
 
 class Button:
     def x():
@@ -50,6 +72,14 @@ def main_screen():
 def main():
     return
 
+screen.fill((100,100,255)) #Baggrundsfarve
+
+ft.Font.render_to(FONT1, screen, pg.Rect(500,50,300,300), "Hello World", BLACK, (255,255,255)) #Viser tekst på "screen"
+    
+# Tekstbokse
+testbox = Textbox(20,20,50,50,CORRECT,screen)
+boxes = [testbox]
+
 #Gameloop test
 while running:
     clock.tick(60) # 60 FPS
@@ -58,9 +88,10 @@ while running:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
-
-    screen.fill((100,100,255)) #Baggrundsfarve
-
+        for box in boxes: #Håndter textbokse
+            box.draw()
+            box.check_events(event)
+    
     # Render
-    ft.Font.render_to(FONT1, screen, pg.Rect(50,50,300,300), "Hello World", BLACK, (255,255,255)) #Viser tekst på "screen"
+    
     disp.flip() #Render næste frame
